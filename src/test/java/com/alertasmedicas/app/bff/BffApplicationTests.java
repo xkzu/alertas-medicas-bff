52c57af94d890cdd2ee22e5c49e45133254eefcf
Kzu <alvaro.vega.pavez@gmail.com>
2025-01-11 08:22:24 -0300

    add: main class test

---
+++ src/test/java/com/alertasmedicas/app/bff/BffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alertasmedicas.app.bff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
