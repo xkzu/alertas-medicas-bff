8235f191e0b283563cc8584ccc7defddd21215ae
Kzu <alvaro.vega.pavez@gmail.com>
2025-01-13 15:47:19 -0300

    update: se implementan metodos getPatientById, addPatient y updatePatient

--- src/main/java/com/alertasmedicas/app/bff/service/PatientService.java
+++ src/main/java/com/alertasmedicas/app/bff/service/PatientService.java
@@ -7,4 +7,10 @@ import java.util.List;
 public interface PatientService {
 
     List<PatientDTO> getPatients();
+
+    List<PatientDTO> getPatientsByIdDoctor(String idDoctor);
+
+    PatientDTO savePatient(PatientDTO patientDTO);
+
+    PatientDTO updatePatient(PatientDTO patientDTO);
 }
--- src/main/java/com/alertasmedicas/app/bff/service/PatientServiceImpl.java
+++ src/main/java/com/alertasmedicas/app/bff/service/PatientServiceImpl.java
@@ -4,7 +4,10 @@ import com.alertasmedicas.app.bff.dto.PatientDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -33,6 +36,49 @@ public class PatientServiceImpl implements PatientService {
                 null,
                 new ParameterizedTypeReference<>() {
                 }
+        );
+        return response.getBody();
+    }
+
+    @Override
+    public List<PatientDTO> getPatientsByIdDoctor(String idDoctor) {
+        String url = domain + "/patient/getPatients/" + idDoctor;
+        ResponseEntity<List<PatientDTO>> response = restTemplate.exchange(
+                url,
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<>() {
+                }
+        );
+        return response.getBody();
+    }
+
+    @Override
+    public PatientDTO savePatient(PatientDTO patientDTO) {
+        String url = domain + "/patient/add";
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<PatientDTO> requestEntity = new HttpEntity<>(patientDTO, headers);
+        ResponseEntity<PatientDTO> response = restTemplate.exchange(
+                url,
+                HttpMethod.POST,
+                requestEntity,
+                PatientDTO.class
+        );
+        return response.getBody();
+    }
+
+    @Override
+    public PatientDTO updatePatient(PatientDTO patientDTO) {
+        String url = domain + "/patient/update";
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<PatientDTO> requestEntity = new HttpEntity<>(patientDTO, headers);
+        ResponseEntity<PatientDTO> response = restTemplate.exchange(
+                url,
+                HttpMethod.PUT,
+                requestEntity,
+                PatientDTO.class
         );
         return response.getBody();
     }
