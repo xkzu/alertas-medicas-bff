ff0f45ee07b23f5bf314ff4b90ed9b876957b08d
Kzu <alvaro.vega.pavez@gmail.com>
2025-01-13 15:46:51 -0300

    update: se implementan metodos getPatientById, addPatient y updatePatient

--- src/main/java/com/alertasmedicas/app/bff/controller/PatientController.java
+++ src/main/java/com/alertasmedicas/app/bff/controller/PatientController.java
@@ -6,6 +6,10 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,11 +31,50 @@ public class PatientController {
     public ResponseEntity<List<PatientDTO>> getAllPatients() {
         log.info("Obteniendo todos las pacientes...");
         try {
-        List<PatientDTO> patients = patientService.getPatients();
+            List<PatientDTO> patients = patientService.getPatients();
             log.info("Lista de pacientes obtenida exitosamente. Total: {} pacientes.", patients.size());
             return ResponseEntity.ok(patients);
         } catch (Exception e) {
             log.error("Error al obtener pacientes: {}", e.getMessage(), e);
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+
+    @GetMapping("/getpatients/{idDoctor}")
+    public ResponseEntity<List<PatientDTO>> getPatientById(@PathVariable("idDoctor") String idDoctor) {
+        log.info("Obteniendo todos las pacientes por id doctor {}", idDoctor);
+        try {
+            List<PatientDTO> patients = patientService.getPatientsByIdDoctor(idDoctor);
+            log.info("Lista de pacientes obtenida exitosamente por id doctor. Total: {} pacientes.", patients.size());
+            return ResponseEntity.ok(patients);
+        } catch (Exception e) {
+            log.error("Error al obtener pacientes por id doctor: {}", e.getMessage(), e);
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<PatientDTO> addPatient(@RequestBody PatientDTO patientDTO) {
+        log.info("Registrando paciente {}", patientDTO);
+        try {
+            PatientDTO patient = patientService.savePatient(patientDTO);
+            log.info("Paciente registrado exitosamente: {}", patient);
+            return ResponseEntity.ok(patient);
+        } catch (Exception e) {
+            log.error("Error al registrar paciente: {}", e.getMessage(), e);
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<PatientDTO> updatePatient(@RequestBody PatientDTO patientDTO) {
+        log.info("Actualizando paciente {}", patientDTO);
+        try {
+            PatientDTO patient = patientService.updatePatient(patientDTO);
+            log.info("Paciente actualizado exitosamente: {}", patient);
+            return ResponseEntity.ok(patient);
+        } catch (Exception e) {
+            log.error("Error al actualizar paciente: {}", e.getMessage(), e);
             return ResponseEntity.internalServerError().build();
         }
     }
