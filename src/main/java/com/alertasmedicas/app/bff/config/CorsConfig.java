da3d89a9988c8e0623e61ce5506854a20b2b8a35
Kzu <alvaro.vega.pavez@gmail.com>
2025-01-13 15:44:31 -0300

    update: se elimina codigo comentado que no se utilizará

--- src/main/java/com/alertasmedicas/app/bff/config/CorsConfig.java
+++ src/main/java/com/alertasmedicas/app/bff/config/CorsConfig.java
@@ -1,14 +1,9 @@
 package com.alertasmedicas.app.bff.config;
 
-import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.NonNull;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -33,22 +28,4 @@ public class CorsConfig {
         };
     }
 
-    // @Bean
-    // CorsConfigurationSource corsConfigurationSource() {
-    //     CorsConfiguration config = new CorsConfiguration();
-
-    //     config.setAllowCredentials(true);
-
-    //     for (String pattern : allowedOrigins) {
-    //         config.addAllowedOriginPattern(pattern);
-    //     }
-
-    //     config.addAllowedHeader("*");
-    //     config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-
-    //     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    //     source.registerCorsConfiguration("/**", config);
-
-    //     return source;
-    // }
 }
